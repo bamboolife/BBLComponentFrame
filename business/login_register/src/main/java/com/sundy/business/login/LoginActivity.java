7fb65bb07dd48204d629bf6b8f62412976f4539e
bamboolife <core_it@163.com>
2019-11-10 15:50:32 +0800

    添加关联

--- business/login_register/src/main/java/com/sundy/business/login/LoginActivity.java
+++ business/login_register/src/main/java/com/sundy/business/login/LoginActivity.java
@@ -3,11 +3,12 @@ package com.sundy.business.login;
 import android.os.Bundle;
 
 
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.sundy.business.R;
 import com.sundy.common.base.BaseActivity;
 
 
-
+@Route(path = "/sundy/login")
 public class LoginActivity extends BaseActivity {
 
 
--- business/welcome/src/main/java/com/sundy/welcome/SplashActivity.java
+++ business/welcome/src/main/java/com/sundy/welcome/SplashActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.sundy.common.base.BaseActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bbl_splash_layout);
+        Observable.interval(3, TimeUnit.SECONDS)
+                .take(1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        navigateToHome();
+                    }
+                });
+    }
+
+    private void navigateToHome() {
+        ARouter.getInstance().build("/sundy/login").greenChannel().navigation();
+        finish();
     }
 }
