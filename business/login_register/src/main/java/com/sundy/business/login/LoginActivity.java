a8b22528569d03ade32b74f9c8be8367806ec8cb
bamboolife <core_it@163.com>
2019-11-10 16:08:33 +0800

    集成butterknife组件化注解

--- business/login_register/src/main/java/com/sundy/business/login/LoginActivity.java
+++ business/login_register/src/main/java/com/sundy/business/login/LoginActivity.java
@@ -1,22 +1,29 @@
 package com.sundy.business.login;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.sundy.business.R;
+import com.sundy.business.R2;
 import com.sundy.common.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 @Route(path = "/sundy/login")
 public class LoginActivity extends BaseActivity {
-
+  @BindView(R2.id.username)
+  EditText etUserName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bbl_login_layout);
-
+        ButterKnife.bind(this);
+         etUserName.setText("我的名字");
     }
 
 
